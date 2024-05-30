5f8b07bb35d251bd331b398cdc1fe255922c668e
Gustavo <gusrot2@gmail.com>
2024-05-30 19:56:41 -0300

    segunda parte

--- src/app/components/paisdetalle/paisdetalle.component.ts
+++ src/app/components/paisdetalle/paisdetalle.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 interface Country {
   name: {
@@ -16,19 +16,32 @@ interface Country {
   standalone: true,
   imports: [CommonModule],
   templateUrl: './paisdetalle.component.html',
-  styleUrl: './paisdetalle.component.css'
+  styleUrls: ['./paisdetalle.component.css']
 })
-export class PaisdetalleComponent implements OnInit {
+export class PaisdetalleComponent implements OnInit, OnChanges {
   @Input() pais: string | null | undefined = null;
   country: Country | null = null;
   
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.cargarPaises();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['pais'] && changes['pais'].currentValue) {
+      this.cargarPaises();
+    }
+  }
+
+  cargarPaises(): void {
     if (this.pais) {
       this.http.get<Country[]>(`https://restcountries.com/v3.1/name/${this.pais}`)
         .subscribe(data => {
           this.country = data[0];
+        }, error => {
+          console.error('Error loading country data:', error);
+          this.country = null;
         });
     }
   }
